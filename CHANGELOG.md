920231eb3efb0c71c9903e184a41d320dbeb60f6
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2023-09-20 17:58:56 +0300

    Make some minor changes to tenant_settings_service

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+# [2.2.5] - 2023-09-20
+- added `limit` attribute to `i_get_by_tenant` and `i_get_by_key` 
+  methods for tenant_settings_service;
+- added method `get` to tenant_settings_service
+
 # [2.2.4] - 2023-09-19
 - Fix unhandled exception in `get_by_id` method in `JobService`
 
--- modular_sdk/services/tenant_settings_service.py
+++ modular_sdk/services/tenant_settings_service.py
@@ -1,7 +1,10 @@
-from modular_sdk.commons import build_response, RESPONSE_BAD_REQUEST_CODE
-from modular_sdk.models.tenant_settings import TenantSettings
+from typing import Optional
 
-from typing import Optional, Iterator
+from pynamodb.pagination import ResultIterator
+
+from modular_sdk.commons import RESPONSE_BAD_REQUEST_CODE
+from modular_sdk.commons import build_response
+from modular_sdk.models.tenant_settings import TenantSettings
 
 RESOURCE_QUOTA = 'RESOURCE_QUOTA'
 
@@ -9,9 +12,17 @@ RESOURCE_QUOTA = 'RESOURCE_QUOTA'
 class TenantSettingsService:
 
     @staticmethod
-    def create(tenant_name: str, key: str, value: Optional[dict] = None):
+    def create(tenant_name: str, key: str, value: Optional[dict] = None
+               ) -> TenantSettings:
         return TenantSettings(
             tenant_name=tenant_name, key=key, value=value or dict()
+        )
+
+    @staticmethod
+    def get(tenant_name: str, key: str) -> Optional[TenantSettings]:
+        return TenantSettings.get_nullable(
+            hash_key=tenant_name,
+            range_key=key
         )
 
     @staticmethod
@@ -48,22 +59,24 @@ class TenantSettingsService:
         return tenant_item[0]
 
     @staticmethod
-    def i_get_by_tenant(tenant: str, key: Optional[str] = None) ->\
-            Iterator[TenantSettings]:
-
+    def i_get_by_tenant(tenant: str, key: Optional[str] = None,
+                        limit: Optional[int] = None
+                        ) -> ResultIterator[TenantSettings]:
         return TenantSettings.query(
             hash_key=tenant,
+            limit=limit,
             range_key_condition=(TenantSettings.key == key) if key else None
         )
 
     @staticmethod
-    def i_get_by_key(key: str, tenant: Optional[str] = None) ->\
-            Iterator[TenantSettings]:
-        # TODO use range_key_condition instead of filter_condition if
-        #  key_tenant_name_index has range_key tenant_name
+    def i_get_by_key(key: str, tenant: Optional[str] = None,
+                     limit: Optional[int] = None
+                     ) -> ResultIterator[TenantSettings]:
+        fc = None
+        if tenant:
+            fc = (TenantSettings.tenant_name == tenant)
         return TenantSettings.key_tenant_name_index.query(
-            hash_key=key, filter_condition=(
-                TenantSettings.tenant_name == tenant
-            ) if tenant else None
+            hash_key=key,
+            limit=limit,
+            filter_condition=fc
         )
-
