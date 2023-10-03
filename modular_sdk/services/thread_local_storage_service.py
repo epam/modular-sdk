eb98111c69546099c428d24bc7de2cbf751cc90d
Bohdan Onsha <bohdan_onsha@epam.com>
2023-10-03 14:06:05 +0300

    update thread_local_storage_service.py

--- modular_sdk/services/thread_local_storage_service.py
+++ modular_sdk/services/thread_local_storage_service.py
@@ -7,17 +7,23 @@ _LOG = get_logger(__name__)
 
 class ThreadLocalStorageService:
     def __init__(self):
-        self.__storage = local()
-        self.__storage.value = {}
+        self.__storage = None
+
+    @property
+    def storage(self):
+        if not self.__storage:
+            self.__storage = local()
+            self.__storage.value = {}
+        return self.__storage.value
 
     def set(self, key: str, value):
         _LOG.debug(f'Setting {key}:{value} to storage')
-        self.__storage.value[key] = value
+        self.storage[key] = value
 
     def get(self, key):
         _LOG.debug(f'Extracting {key} var from storage')
-        return self.__storage.value.get(key)
+        return self.storage.get(key)
 
     def pop(self, key):
         _LOG.debug(f'Pop {key} var from storage')
-        return self.__storage.value.pop(key, None)
+        return self.storage.pop(key, None)
